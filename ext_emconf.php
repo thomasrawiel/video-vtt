bfe40421994400e3d50f27947abf15cefc869c90
thomasrawiel <thomasrawiel@users.noreply.github.com>
2021-09-28 18:56:56 +0200

    test TYPO3 11 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.3',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.0.0-10.99.99',
+            'typo3' => '10.0.0-11.99.99',
         ],
         'conflicts' => [
         ],
