981be8915f4d6cf0540a8d9b66c7aaeb105417b5
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2025-07-24 14:26:02 +0200

    [feat] add vtt to allowed text files (change #106240)

--- ext_localconf.php
+++ ext_localconf.php
@@ -13,4 +13,9 @@ call_user_func(function () {
     $rendererRegistry->registerRendererClass(
         TRAW\VideoVtt\Resource\Rendering\VideoTagRenderer::class
     );
+    if (isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['textfile_ext'])) {
+        $txt = array_filter(array_map('trim', explode(',', $GLOBALS['TYPO3_CONF_VARS']['SYS']['textfile_ext'])));
+        $txt[] = 'vtt';
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['textfile_ext'] = implode(',', array_unique($txt));
+    }
 });
